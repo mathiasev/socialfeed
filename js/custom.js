51e4595e3029f07bbd9a619de3e298ddb3474a82
mathiasev <mathiasev@hotmail.com>
2017-08-19 21:19:51 +1000

    Time to switch to js

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,8 @@
 if(window.location.hash) {
   // Fragment exists
   var hash = window.location.hash;
-  
-console.log(hash.substring(1));
-    $.get("https://api.instagram.com/v1/users/self/follows?" + hash, function(data, status){
+  var url = "https://api.instagram.com/v1/users/self/follows?" + hash.substring(1);
+    $.get(url, function(data, status){
         $('#elasticstack').html("Data: " + data + "\nStatus: " + status);
     });  
   
