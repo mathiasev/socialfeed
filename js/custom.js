75d977e55df40b59fe0e01355c50a9bd9569af61
mathiasev <mathiasev@hotmail.com>
2017-08-19 21:14:52 +1000

    Time to switch to js

--- js/custom.js
+++ js/custom.js
@@ -5,9 +5,7 @@ if(window.location.hash) {
 
     $.get("https://api.instagram.com/v1/users/self/follows?" + hash, function(data, status){
         $('#elasticstack').html("Data: " + data + "\nStatus: " + status);
-    });
-});
-  
+    });  
   
 } else {
 	$('#elasticstack').html('<li><a href="https://api.instagram.com/oauth/authorize/?client_id=ddc788c63b2a444ca2898f6acaa88780&redirect_uri=http://13.59.66.63/socialfeed/&response_type=token&scope=basic+public_content+follower_list+comments+relationships+likes" id="InstagramLogin">Log into Instagram</a></li>');
