62b87d483fd3d45554dacf685704b762048efe73
mathiasev <mathiasev@hotmail.com>
2017-08-19 21:13:50 +1000

    Time to switch to js

--- index.php
+++ index.php
@@ -1,52 +1,4 @@
-<?php 
-$stack = new Stack();
-
-class Stack {
-	private $instagramAccessToken = 'UNAUTHOURISED';
-	private $alive = false;
-
-	function isAlive() {
-		return $this->alive;
-	}
-	
-	function setAuthCode($authCode) {
-		$this->instagramAccessToken = $authCode;
-	}
-	
-	
-	function getPosts() {
-		
-		$friends = getFriends();
-		
-	}
-	
-	private function getFriends() {
-	$friends = getEndpoint('https://api.instagram.com/v1/users/self/follows');
-	print_r($friends);
-}
-
-/* ---- Set up CURL ---- */
-private function getEndpoint($req_url) {
-	$ch = curl_init();
-	    curl_setopt($ch, CURLOPT_URL, $req_url . '?access_token=' . $this->instagramAccessToken);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	
-    $jsonData = curl_exec($ch);
-    curl_close($ch);
-	$output = json_decode($jsonData);
-    return $output;
-}
-
-}
-
-
-
-
-
-
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en" class="no-js">
 	<head>
 		<meta charset="UTF-8" />
@@ -64,19 +16,21 @@ private function getEndpoint($req_url) {
 		<div class="container">
 			<!-- Top Navigation -->
 			<ul id="elasticstack" class="elasticstack">
-				<?php
-					if( isset($_GET['access_token']) ): 
+				
+					<!--if( isset($_GET['access_token']) ): 
 						$stack->setAuthCode($_GET['access_token']); 
 					else:
-						echo '<li><a href="https://api.instagram.com/oauth/authorize/?client_id=ddc788c63b2a444ca2898f6acaa88780&redirect_uri=http://13.59.66.63/socialfeed/&response_type=token&scope=basic+public_content+follower_list+comments+relationships+likes" id="InstagramLogin">Log into Instagram</a></li>'; 
+						echo ''; 
 					endif; 
 					
-					if($stack->isAlive()): $stack->getPosts(); endif; 
-				?>
+					if($stack->isAlive()): $stack->getPosts(); endif; -->
+				
 			</ul>
 		</div><!-- /container -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="js/draggabilly.pkgd.min.js"></script>
 		<script src="js/elastiStack.js"></script>
+		<script src="js/custom.js"></script>
 		<script>
 			new ElastiStack( document.getElementById( 'elasticstack' ) );
 		</script>
