b49c8a6a7b94c6fd9150bfd330b1b87a67e30ce9
mathiasev <mathiasev@hotmail.com>
2017-08-19 21:41:17 +1000

    Javascript

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@ if(window.location.hash) {
 })
   .done(function( data ) {
 	  
-	  for (var key in data[0]) {
+	  for (var key in data[1]) {
     // skip loop if the property is from prototype
     if (!data.hasOwnProperty(key)) continue;
 
