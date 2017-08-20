ce30277d6dde9df4f6a0847227d07dc58c96ba23
mathiasev <mathiasev@hotmail.com>
2017-08-20 13:06:50 +1000

    Facebook API

--- js/custom.js
+++ js/custom.js
@@ -32,39 +32,51 @@
     return false;
   };
 
-
-
-/* SIGN IN WITH INSTAGRAM  and GET DATA */
-var cookies = document.cookie;
-if( cookies) {
-	console.log(cookies);
-	var token = getCookie('instagramaccess_token');
-	token = "access_token=" + token; 
-	  var goURL = 'go.php?token=' + token;
-  $.ajax({
-	  url: goURL,
-	  type: 'POST'
-  })
-  .done(function( data ) {
-	  
-
-   $('#elasticstack').html(data);
-new ElastiStack( document.getElementById( 'elasticstack' ) );
-  });
- 
- 
-}
-else if(window.location.hash) {
+/* Set Instagram cookie if code in url */
+if(window.location.hash) {
   // Fragment exists
   var hash = window.location.hash;
   
   document.cookie = "instagram" + hash.substring(1);
+  
  
-} else {
-	$('#elasticstack').html('<li><a href="https://api.instagram.com/oauth/authorize/?client_id=ddc788c63b2a444ca2898f6acaa88780&redirect_uri=http://13.59.66.63/socialfeed/&response_type=token&scope=basic+public_content+follower_list+comments+relationships+likes" id="InstagramLogin">Log into Instagram</a></li>');
+} 
+ 
+var cookies = document.cookie;
+if(cookies) { 
+	
+	var instagramaccess_token = getCookie('instagramaccess_token');
+	var facebookaccess_token = getCookie('fbsr_879355528897757');
+	
+	if (facebookaccess_token != '' && instagramaccess_token != '') {
+		
+		// Get Posts
+		var token = "instagram_token=" + instagramaccess_token; 
+			token += "facebook_token=" + facebookaccess_token;
+		var goURL = 'go.php?' + token;
+		
+		$.ajax({
+		  url: goURL,
+		  type: 'POST'
+		})
+		.done(function( data ) {
+			$('#elasticstack').html(data);
+			new ElastiStack( document.getElementById( 'elasticstack' ) );
+		});
 
+	}
+	else if (facebookaccess_token == '') {
+		// Sign in with Facebook 
+		$('#elasticstack').html('<li><p><a href="#" onClick="logInWithFacebook()">Log In to Facebook</a></p></li>');
+
+	}
 }
-
+else {
+	// Get Instagram sign in
+		$('#elasticstack').html('<li><p><a href="https://api.instagram.com/oauth/authorize/?client_id=ddc788c63b2a444ca2898f6acaa88780&redirect_uri=http://13.59.66.63/socialfeed/&response_type=token&scope=basic+public_content+follower_list+comments+relationships+likes" id="InstagramLogin">Log into Instagram</a></p></li>');
+}
+	
+	
 
 function getCookie(name) {
   var value = "; " + document.cookie;
