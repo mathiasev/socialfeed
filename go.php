919c61c60d4f67fef96acd7ad23b39dda6ac7ebd
mathiasev <mathiasev@hotmail.com>
2017-08-20 11:07:17 +1000

    Let's get classy

--- go.php
+++ go.php
@@ -2,21 +2,38 @@
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$accessToken = (isset($_GET['token'])) ? $_GET['token'] : 'No Access Token';
 
-function getEndpoint($req_url) {
-	global $accessToken;
-        $ch = curl_init(); 
-		echo $authURL = $req_url . '?' . $accessToken;
+$stack = new Stack();
+
+$stack->setAccessToken((isset($_GET['token'])) ? $_GET['token'] : 'No Access Token');
+
+class Stack {
+	private $instaAccessToken = 'No Token Set';
+	
+	function setAccessToken($accessToken) {
+		$this->instaAccessToken = $accessToken;
+	}
+	
+	private function getEndpoint($req_url) {
+		$authURL = $req_url . '?' . $this->instaAccessToken;
+        
+		$ch = curl_init(); 
         curl_setopt($ch, CURLOPT_URL, $authURL); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
         $output = curl_exec($ch); 
         curl_close($ch);
-    return json_decode($output);
+		
+		return json_decode($output);
+	}
+	
+	function getProfile() {
+		$me = $this->getEndpoint('https://api.instagram.com/v1/users/self');
+		$me = $me->data;
+		echo sprintf('<li><img src="%s" alt="%s"><h3>Welcome %s</h3></li>', $me->profile_picture, $me->username, $me->full_name);
+	}
 }
 
-$me = getEndpoint('https://api.instagram.com/v1/users/self');
-$me = $me->data;
-echo sprintf('<li><img src="%s" alt="%s"><h3>Welcome %s</h3></li>', $me->profile_picture, $me->username, $me->full_name);
-print_r($me);
+$stack->getProfile();
+
+
 ?>
