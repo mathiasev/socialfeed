1b64da97b09134e9b5c403a3283fff01d00a1f83
mathiasev <mathiasev@hotmail.com>
2017-08-20 12:41:15 +1000

    Cookie time

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,9 @@
 var cookies = document.cookie;
 if( cookies) {
 	console.log(cookies);
-	  var goURL = 'go.php?token=' + hash.substring(1);
+	var token = getCookie(instagramaccess_token);
+	token = "access_token=" + token; 
+	  var goURL = 'go.php?token=' + token;
   $.ajax({
 	  url: goURL,
 	  type: 'POST'
@@ -26,4 +28,11 @@ else if(window.location.hash) {
 } else {
 	$('#elasticstack').html('<li><a href="https://api.instagram.com/oauth/authorize/?client_id=ddc788c63b2a444ca2898f6acaa88780&redirect_uri=http://13.59.66.63/socialfeed/&response_type=token&scope=basic+public_content+follower_list+comments+relationships+likes" id="InstagramLogin">Log into Instagram</a></li>');
 
+}
+
+
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
 }
