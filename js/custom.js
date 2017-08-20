ab624ae04482811009de153eb2677e48eef5bcfb
mathiasev <mathiasev@hotmail.com>
2017-08-20 12:38:03 +1000

    Cookie time

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,8 @@
-if(window.location.hash) {
-  // Fragment exists
-  var hash = window.location.hash;
-  
-  document.cookie = "instagram" + hash.substring(1);
- 
-} else {
-	$('#elasticstack').html('<li><a href="https://api.instagram.com/oauth/authorize/?client_id=ddc788c63b2a444ca2898f6acaa88780&redirect_uri=http://13.59.66.63/socialfeed/&response_type=token&scope=basic+public_content+follower_list+comments+relationships+likes" id="InstagramLogin">Log into Instagram</a></li>');
 
-}
 
 var cookies = document.cookie;
 if( cookies) {
-	
+	console.log(cookies);
 	  var goURL = 'go.php?token=' + hash.substring(1);
   $.ajax({
 	  url: goURL,
@@ -25,4 +16,14 @@ new ElastiStack( document.getElementById( 'elasticstack' ) );
   });
  
  
+}
+else if(window.location.hash) {
+  // Fragment exists
+  var hash = window.location.hash;
+  
+  document.cookie = "instagram" + hash.substring(1);
+ 
+} else {
+	$('#elasticstack').html('<li><a href="https://api.instagram.com/oauth/authorize/?client_id=ddc788c63b2a444ca2898f6acaa88780&redirect_uri=http://13.59.66.63/socialfeed/&response_type=token&scope=basic+public_content+follower_list+comments+relationships+likes" id="InstagramLogin">Log into Instagram</a></li>');
+
 }
