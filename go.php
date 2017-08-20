e70b62a9ab57931fe60d2c744a434f022f34f66e
mathiasev <mathiasev@hotmail.com>
2017-08-20 10:57:15 +1000

    Is the auth attached

--- go.php
+++ go.php
@@ -1,4 +1,6 @@
-<?php 
+<?php ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $accessToken = (isset($_GET['token'])) ? $_GET['token'] : 'No Access Token';
 
