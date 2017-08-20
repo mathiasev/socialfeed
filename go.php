0a17cf6e779ee9fb9b7c8e4aa3c8518862918939
mathiasev <mathiasev@hotmail.com>
2017-08-20 10:31:56 +1000

    Move to internal php

--- go.php
+++ go.php
@@ -1,8 +1,18 @@
 <?php 
-print_r($_GET);
 
-echo '\n\n\n\n\n';
-print_r($_POST);
+$accessToken = (isset($_GET['token')) ? $_GET['token'] : 'No Access Token';
 
+function getEndpoint($req_url) {
+        $ch = curl_init(); 
+		$authURL = $req_url . '?' . $accessToken;
+        curl_setopt($ch, CURLOPT_URL, $authURL); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        $output = curl_exec($ch); 
+        curl_close($ch);
+    return json_decode($output);
+}
 
+$me = getEndpoint('https://api.instagram.com/v1/users/self');
+
+print_r($me);
 ?>
