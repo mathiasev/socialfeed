b72fe57080b39ae8ae6bc3c3f80ff2d059384008
mathiasev <mathiasev@hotmail.com>
2017-08-21 22:06:37 +1000

    Session stack

--- go.php
+++ go.php
@@ -1,17 +1,16 @@
 <?php require ('class.php');
- $stack = $_SESSION['STACK'];
- print_r($stack);
+ 
  
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-if (isset($_GET['access_token'])) : $stack->setInstagramAccessToken($_GET['access_token']); endif;
-if (isset($_GET['getPosts'])) 	  :	$stack->getProfile();
-									$stack->getPosts();										endif;
+if (isset($_GET['access_token'])) : $stack = $_SESSION['STACK']; $stack->setInstagramAccessToken($_GET['access_token']); $_SESSION['STACK'] = $stack; endif;
+if (isset($_GET['getPosts'])) 	  :	$stack = $_SESSION['STACK']; $stack->getProfile();
+									$stack->getPosts();										$_SESSION['STACK'] = $stack;endif;
 									
 
 
-$_SESSION['STACK'] = $stack;
+
 ?>
