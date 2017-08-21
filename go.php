c6cbfc785f3001183943186092228f2a0d00ed07
mathiasev <mathiasev@hotmail.com>
2017-08-21 21:51:38 +1000

    Facebook API

--- go.php
+++ go.php
@@ -1,7 +1,7 @@
 <?php ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include 'class.php' 
+include 'class.php';
 
 
 if (isset($_GET['access_token'])) : $stack->setInstagramAccessToken($_GET['access_token']); endif;
