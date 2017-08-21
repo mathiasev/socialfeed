0a30781132ae8920c527e1b7e3de5458cc18b763
mathiasev <mathiasev@hotmail.com>
2017-08-21 21:38:54 +1000

    Facebook API

--- go.php
+++ go.php
@@ -106,7 +106,7 @@ class Post {
 }
 
 $stack = new Stack();
-$stack->setInstagramAccessToken((isset($_GET['instagram_token'])) ? $_GET['instagram_token'] : 'No Access Token');
+$stack->setInstagramAccessToken((isset($_GET['instagramaccess_token'])) ? $_GET['instagramaccess_token'] : 'No Access Token');
 $stack->getProfile();
 $stack->getPosts();
 
--- js/home.js
+++ js/home.js
@@ -1,23 +1,29 @@
 /* Set Instagram cookie if code in url */
 if(window.location.hash) {
   // Fragment exists
-  var hash = window.location.hash;
-  var token = hash.substring(1);
+	var hash = window.location.hash;
+	var token = hash.substring(1);
   
-var goURL = '../go.php?instagram' + token;
-		console.log(goURL);
-		$.ajax({
-		  url: goURL,
-		  type: 'POST'
-		})
-		.done(function( data ) {
-			window.location.hash = "";
-console.log("Ready");
-		});
+	var goURL = '../go.php?instagram' + token;
+	
+	$.ajax({
+		url: goURL,
+		type: 'POST'
+	})
+	.done(function( data ) {
+		window.location.hash = "";
+		getPOSTS();
+	});
 }
 
-  function getCookie(name) {
-  var value = "; " + document.cookie;
-  var parts = value.split("; " + name + "=");
-  if (parts.length == 2) return parts.pop().split(";").shift();
+
+function getPOSTS() {
+	$.ajax({
+	url: '../go.php?getPosts=true',
+	  type: 'POST'
+	})
+	.done(function( data ) {
+		$('#elasticstack').html(data);
+	new ElastiStack( document.getElementById( 'elasticstack' ) );
+	});
 }
