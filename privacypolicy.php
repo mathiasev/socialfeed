56236c08e0095818027a1f3c81d0ec8bab85dbb5
mathiasev <mathiasev@hotmail.com>
2017-08-22 12:43:39 +1000

    Facebook Contents

--- privacypolicy.php
+++ privacypolicy.php
@@ -15,132 +15,70 @@
 	</head>
 	<body>
 		<div class="container">
-			<!-- Top Navigation -->
-			<ul id="elasticstack" class="elasticstack">
-				<li>
-				PRIVACY STATEMENT
+			<div class="">
+				<h1>PRIVACY STATEMENT</h1>
 
 ----
 
-SECTION 1 - WHAT DO WE DO WITH YOUR INFORMATION?
+<h2>SECTION 1 - WHAT DO WE DO WITH YOUR INFORMATION?</h2>
 
-When you purchase something from our store, as part of the buying and selling process, we collect the personal information you give us such as your name, address and email address.
+<p>When you use this site, you do not provide Stack with any personal information. You login directly with Facebook and Instagram, and once you have finished your session, no data is kept.</p>
 
-When you browse our store, we also automatically receive your computer’s internet protocol (IP) address in order to provide us with information that helps us learn about your browser and operating system.
+<h2>SECTION 2 - CONSENT</h2>
 
-Email marketing (if applicable): With your permission, we may send you emails about our store, new products and other updates.
+<strong>How do you get my consent?</strong>
 
+When you login with Facebook or Instagram, you login directly with the social newtork. Stack will then be given a code to access your data whilst you are using the site. This code will expire when you finish using the site.</p>
+<p>If we ask for your personal information for a secondary reason, like marketing, we will either ask you directly for your expressed consent, or provide you with an opportunity to say no.</p>
 
-SECTION 2 - CONSENT
+<strong>How do I withdraw my consent?</strong>
 
-How do you get my consent?
+<p>If after you opt-in, you change your mind, you may withdraw your consent for us to contact you, for the continued collection, use or disclosure of your information, at anytime, by simply stopping to use the site.</p>
 
-When you provide us with personal information to complete a transaction, verify your credit card, place an order, arrange for a delivery or return a purchase, we imply that you consent to our collecting it and using it for that specific reason only.
 
-If we ask for your personal information for a secondary reason, like marketing, we will either ask you directly for your expressed consent, or provide you with an opportunity to say no.
+<h2>SECTION 3 - DISCLOSURE</h2>
 
+<p>We may disclose your personal information if we are required by law to do so or if you abuse this service.</p>
 
-How do I withdraw my consent?
 
-If after you opt-in, you change your mind, you may withdraw your consent for us to contact you, for the continued collection, use or disclosure of your information, at anytime, by contacting us at creatednew@mathiaseverson.com or mailing us at: Created New 36 Warrego CCT, Kaleen, ACT, 2617, Australia
+<h2>SECTION 5 - THIRD-PARTY SERVICES</h2>
 
 
-SECTION 3 - DISCLOSURE
+<p>In general, the third-party providers used by us will only collect, use and disclose your information to the extent necessary to allow them to perform the services they provide to us.</p>
 
-We may disclose your personal information if we are required by law to do so or if you violate our Terms of Service.
+<p>However, certain third-party service providers, such as payment gateways and other payment transaction processors, have their own privacy policies in respect to the information we are required to provide to them for your purchase-related transactions.</p>
 
+<p>For these providers, we recommend that you read their privacy policies so you can understand the manner in which your personal information will be handled by these providers.</p>
 
-SECTION 4 - SHOPIFY
+<p>In particular, remember that certain providers may be located in or have facilities that are located in a different jurisdiction than either you or us. So if you elect to proceed with a transaction that involves the services of a third-party service provider, then your information may become subject to the laws of the jurisdiction(s) in which that service provider or its facilities are located.</p>
 
-Our store is hosted on Shopify Inc. They provide us with the online e-commerce platform that allows us to sell our products and services to you.
+<p>As an example, if you are located in Canada and your transaction is processed by a payment gateway located in the United States, then your personal information used in completing that transaction may be subject to disclosure under United States legislation, including the Patriot Act.</p>
 
-Your data is stored through Shopify’s data storage, databases and the general Shopify application. They store your data on a secure server behind a firewall.
+<p>Once you leave this website or are redirected to a third-party website or application, you are no longer governed by this Privacy Policy or our website’s Terms of Service.</p>
 
 
-Payment:
+<strong>Links</strong>
 
-If you choose a direct payment gateway to complete your purchase, then Shopify stores your credit card data. It is encrypted through the Payment Card Industry Data Security Standard (PCI-DSS). Your purchase transaction data is stored only as long as is necessary to complete your purchase transaction. After that is complete, your purchase transaction information is deleted.
+<p>When you click on links on our store, they may direct you away from our site. We are not responsible for the privacy practices of other sites and encourage you to read their privacy statements.</p>
 
-All direct payment gateways adhere to the standards set by PCI-DSS as managed by the PCI Security Standards Council, which is a joint effort of brands like Visa, MasterCard, American Express and Discover.
 
-PCI-DSS requirements help ensure the secure handling of credit card information by our store and its service providers.
+<h2>SECTION 6 - SECURITY</h2>
 
-For more insight, you may also want to read Shopify’s Terms of Service here or Privacy Statement here.
+<p>To protect your personal information, we take reasonable precautions and follow industry best practices to make sure it is not inappropriately lost, misused, accessed, disclosed, altered or destroyed.</p>
 
 
-SECTION 5 - THIRD-PARTY SERVICES
+<h2>SECTION 7 - AGE OF CONSENT</h2>
 
+<p>By using this site, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this site.</p>
 
-In general, the third-party providers used by us will only collect, use and disclose your information to the extent necessary to allow them to perform the services they provide to us.
 
-However, certain third-party service providers, such as payment gateways and other payment transaction processors, have their own privacy policies in respect to the information we are required to provide to them for your purchase-related transactions.
+<h2>SECTION 8 - CHANGES TO THIS PRIVACY POLICY</h2>
 
-For these providers, we recommend that you read their privacy policies so you can understand the manner in which your personal information will be handled by these providers.
+<p>We reserve the right to modify this privacy policy at any time, so please review it frequently. Changes and clarifications will take effect immediately upon their posting on the website. If we make material changes to this policy, we will notify you here that it has been updated, so that you are aware of what information we collect, how we use it, and under what circumstances, if any, we use and/or disclose it.</p>
 
-In particular, remember that certain providers may be located in or have facilities that are located in a different jurisdiction than either you or us. So if you elect to proceed with a transaction that involves the services of a third-party service provider, then your information may become subject to the laws of the jurisdiction(s) in which that service provider or its facilities are located.
+<p>If our store is acquired or merged with another company, your information may be transferred to the new owners so that we may continue to sell products to you.</p>
 
-As an example, if you are located in Canada and your transaction is processed by a payment gateway located in the United States, then your personal information used in completing that transaction may be subject to disclosure under United States legislation, including the Patriot Act.
-
-Once you leave our store’s website or are redirected to a third-party website or application, you are no longer governed by this Privacy Policy or our website’s Terms of Service.
-
-
-Links
-
-When you click on links on our store, they may direct you away from our site. We are not responsible for the privacy practices of other sites and encourage you to read their privacy statements.
-
-
-SECTION 6 - SECURITY
-
-To protect your personal information, we take reasonable precautions and follow industry best practices to make sure it is not inappropriately lost, misused, accessed, disclosed, altered or destroyed.
-
-If you provide us with your credit card information, the information is encrypted using secure socket layer technology (SSL) and stored with a AES-256 encryption.  Although no method of transmission over the Internet or electronic storage is 100% secure, we follow all PCI-DSS requirements and implement additional generally accepted industry standards.
-
-
-COOKIES
-
-Here is a list of cookies that we use. We’ve listed them here so you can choose if you want to opt-out of cookies or not.
-
-_session_id, unique token, sessional, Allows Shopify to store information about your session (referrer, landing page, etc).
-
-_shopify_visit, no data held, Persistent for 30 minutes from the last visit, Used by our website provider’s internal stats tracker to record the number of visits
-
-_shopify_uniq, no data held, expires midnight (relative to the visitor) of the next day, Counts the number of visits to a store by a single customer.
-
-cart, unique token, persistent for 2 weeks, Stores information about the contents of your cart.
-
-_secure_session_id, unique token, sessional
-
-storefront_digest, unique token, indefinite If the shop has a password, this is used to determine if the current visitor has access.
-
-
-
-SECTION 7 - AGE OF CONSENT
-
-By using this site, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this site.
-
-
-SECTION 8 - CHANGES TO THIS PRIVACY POLICY
-
-We reserve the right to modify this privacy policy at any time, so please review it frequently. Changes and clarifications will take effect immediately upon their posting on the website. If we make material changes to this policy, we will notify you here that it has been updated, so that you are aware of what information we collect, how we use it, and under what circumstances, if any, we use and/or disclose it.
-
-If our store is acquired or merged with another company, your information may be transferred to the new owners so that we may continue to sell products to you.
-
-
-QUESTIONS AND CONTACT INFORMATION
-
-If you would like to: access, correct, amend or delete any personal information we have about you, register a complaint, or simply want more information contact our Privacy Compliance Officer at creatednew@mathiaseverson.com or by mail at Created New
-
-[Re: Privacy Compliance Officer]
-
-[36 Warrego CCT, Kaleen, ACT, 2617, Australia]
-
-----
-
-				
-				
-				</li>
-				
-			</ul>
+			</div>
 		</div><!-- /container -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="js/draggabilly.pkgd.min.js"></script>
