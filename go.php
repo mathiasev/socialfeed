a8d570f65fb45a684a727620e647c9a8271a884c
mathiasev <mathiasev@hotmail.com>
2017-08-25 13:40:25 +1000

    Facebook Load

--- go.php
+++ go.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+echo '<!--'; print_r($_SESSION); echo '-->';
 
 if (isset($_GET['access_token'])) 	: 	$stack = $_SESSION['STACK']; 
 										$stack->setInstagramAccessToken($_GET['access_token']); 
